744d39a1bdb9c194689b37b15f91e0d8490abbf5
Kaveen_Wijethunga <it20185770@my.sliit.lk>
2024-12-24 17:41:32 +0530

    upload outcome screen ui completed

--- src/screens/UploadOutcome/UploadOutcome.tsx
+++ src/screens/UploadOutcome/UploadOutcome.tsx
@@ -15,6 +15,7 @@ import CustomIcon from 'components/CustomIcon';
 import style from './style';
 import {useNavigation} from '@react-navigation/native';
 import Header from 'components/Header/Header';
+import DatePicker from 'react-native-date-picker';
 
 const UploadOutcome = () => {
   const navigation = useNavigation();
@@ -22,6 +23,19 @@ const UploadOutcome = () => {
   const [feedBack, setFeedBack] = useState('');
   const [comments, setComments] = useState('');
   const [geoLocation, setGeoLocation] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [open, setOpen] = useState(false);
+  const options = {timeZone: 'Asia/Colombo'};
+  const currentDate = date.toLocaleDateString('en-US', options);
+  const dateString = currentDate;
+  const dateParts = dateString.split('/');
+  const year = parseInt(dateParts[2]);
+  const formattedDate = postponedDate
+    ? `${year}-${dateParts[0].padStart(2, '0')}-${dateParts[1].padStart(
+        2,
+        '0',
+      )}`
+    : '';
 
   return (
     <SafeAreaView style={style.container}>
@@ -35,14 +49,40 @@ const UploadOutcome = () => {
         style={{flex: 1}}>
         <ScrollView contentContainerStyle={{flexGrow: 1}}>
           <View style={style.innerContainer}>
-            <InputText
-              value={postponedDate}
-              onChange={setPostponedDate}
-              label="Postponed Date"
-              labelFontSize={18}
-              labelFontWeight="bold"
-              placeholderFontSize={16}
-            />
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+              <InputText
+                value={formattedDate}
+                onChange={setPostponedDate}
+                label="Postponed Date"
+                labelFontSize={18}
+                labelFontWeight="bold"
+                placeholderFontSize={16}
+                editable={false}
+              />
+              <TouchableOpacity onPress={() => setOpen(true)}>
+                <CustomIcon
+                  icon={'calendar-sharp'}
+                  type={'Ionicons'}
+                  size={25}
+                  color={'#0B8FAC'}
+                  style={{marginLeft: -45, marginTop: 18}}
+                />
+              </TouchableOpacity>
+              <DatePicker
+                modal
+                open={open}
+                date={date}
+                mode="date"
+                onConfirm={(date: any) => {
+                  setOpen(false);
+                  setPostponedDate(date);
+                  setDate(date);
+                }}
+                onCancel={() => {
+                  setOpen(false);
+                }}
+              />
+            </View>
 
             <InputText
               value={feedBack}
@@ -79,7 +119,7 @@ const UploadOutcome = () => {
               <ActionButton
                 title={'Add'}
                 customStyle={{
-                  marginTop: 90,
+                  marginTop: 110,
                   marginBottom: 40,
                   width: '60%',
                 }}
